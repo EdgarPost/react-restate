9bd99765ffcf4cd82f8e8e56c46656375efcca71
Edgar Post <epost@anwb.nl>
2021-04-24 15:02:09 +0200

    Update outdated packages

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,7 @@
 export default {
 	clearMocks: true,
 	collectCoverage: true,
-	collectCoverageFrom: [
-		"**/*.{ts,tsx}",
-		"!**/node_modules/**",
-		"!**/vendor/**",
-	],
+	collectCoverageFrom: ["**/*.{ts,tsx}", "!**/node_modules/**", "!**/dist/**"],
 	coverageDirectory: "<rootDir>coverage",
 	coverageReporters: ["json", "text", "lcov", "clover"],
 };
