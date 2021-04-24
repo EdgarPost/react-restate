4e3f1b28d38643ecbbe96a7b588d6d9e1ac4c538
Edgar Post <epost@anwb.nl>
2021-04-24 15:08:23 +0200

    Ignore coverage/

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,12 @@
 export default {
 	clearMocks: true,
 	collectCoverage: true,
-	collectCoverageFrom: ["**/*.{ts,tsx}", "!**/node_modules/**", "!**/dist/**"],
+	collectCoverageFrom: [
+		"**/*.{ts,tsx}",
+		"!**/node_modules/**",
+		"!**/dist/**",
+		"!**/coverage/**",
+	],
 	coverageDirectory: "<rootDir>coverage",
 	coverageReporters: ["json", "text", "lcov", "clover"],
 };
